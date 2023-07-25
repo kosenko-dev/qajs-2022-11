f7ca809c2c7f6a0816fa84684e0bb0e6d8a9f5b3
kosenko.dev <d.kosenko@qiwi.com>
2023-07-25 16:32:03 +0300

    #6 Пробуем unit тесты своими руками

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -5,6 +5,33 @@ import { nameIsValid, fullTrim, getTotal } from '../src/app.js'
 /**
  * Для проверки, что jest настроен правильно. Можно удалить
  */
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3)
+test('Successful test for trimming spaces', () => {
+  expect(fullTrim(' arm ')).toBe('arm')
+})
+
+test('Successful test for empty string', () => {
+  expect(fullTrim('')).toBe('')
+})
+
+test('Successful test with no spaces', () => {
+  expect(fullTrim('arm')).toBe('arm')
+})
+
+const nameIsValidTestData = [
+  { name: 'Ivan', isValid: true },
+  { name: 'ad', isValid: true },
+  { name: 'a', isValid: false },
+  { name: '', isValid: false }
+]
+test.each(nameIsValidTestData)('Tests that name=$name validation is equal to $isValid', ({ name, isValid }) => {
+  expect(nameIsValid(name)).toBe(isValid)
+})
+
+test.each`
+    items                             | discount  | expected
+    ${[{ price: 10, quantity: 10 }]}  | ${10}     | ${90}
+    ${[{ price: 0, quantity: 10 }]}   | ${10}     | ${0}
+    ${[{ price: 10, quantity: 0 }]}   | ${50}     | ${0}
+  `('Tests that items: $items with $disount in total cost $expected', ({ items, discount, expected }) => {
+  expect(getTotal(items, discount)).toBe(expected)
 })
